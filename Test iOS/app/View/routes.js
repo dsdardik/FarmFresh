d9f31c93403f4b887772af8ed321a8e85e1ad35f
Michael Donohue <mike8875@gmail.com>
2016-12-06 23:56:43 -0500

    Deleted Old Project Created new one

---
+++ Test iOS/app/View/routes.js
@@ -0,0 +1,43 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+    .when('/home', {
+        templateUrl: 'landing.html',
+        //controller: 'buttonController',
+    }).when('/map', {
+        templateUrl: 'map.html',
+        //controller: 'buttonController',
+    }).when('/news', {
+        templateUrl: 'news.html',
+        //controller: 'buttonController',
+    });
+    
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+//Controllers
+
+//BlankController
+(function () {
+    'use strict';
+
+    app.controller('blankController', BlankController);
+    BlankController.$inject = ['$scope'];
+
+    function ButtonController($scope) {
+        var vm = this;
+        vm.var = false;
+       
+
+        function clickButton(input) {
+            vm.output = !input;
+        };
+
+    }
+
+
+})();
+
