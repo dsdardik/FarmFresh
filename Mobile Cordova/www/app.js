16155fc0d804096e21b9902dabc50ac829dc9d16
mike8875 <mike8875@gmail.com>
2017-04-18 23:27:57 -0400

    installed inappbrowser and linked social media accounts. Changed landing page

--- Mobile Cordova/www/app.js
+++ Mobile Cordova/www/app.js
@@ -85,6 +85,63 @@ app.config(function ($routeProvider) {
             vm.back = false;
             vm.part = false;
         }
+
+
+        vm.openInsta = function() {
+            var url = 'https://www.instagram.com/auerfarm/';
+            var target = '_blank';
+            var options = "location=yes"
+            var ref = cordova.InAppBrowser.open(url, target, options);
+
+            ref.addEventListener('loadstart', loadstartCallback);
+            ref.addEventListener('loadstop', loadstopCallback);
+            ref.addEventListener('loadloaderror', loaderrorCallback);
+            ref.addEventListener('exit', exitCallback);
+
+            function loadstartCallback(event) {
+                console.log('Loading started: ' + event.url)
+            }
+
+            function loadstopCallback(event) {
+                console.log('Loading finished: ' + event.url)
+            }
+
+            function loaderrorCallback(error) {
+                console.log('Loading error: ' + error.message)
+            }
+
+            function exitCallback() {
+                console.log('Browser is closed...')
+            }
+        }
+
+        vm.openFB = function () {
+            var url = 'https://www.facebook.com/auerfarm.org/?ref=br_rs';
+            var target = '_blank';
+            var options = "location=yes"
+            var ref = cordova.InAppBrowser.open(url, target, options);
+
+            ref.addEventListener('loadstart', loadstartCallback);
+            ref.addEventListener('loadstop', loadstopCallback);
+            ref.addEventListener('loadloaderror', loaderrorCallback);
+            ref.addEventListener('exit', exitCallback);
+
+            function loadstartCallback(event) {
+                console.log('Loading started: ' + event.url)
+            }
+
+            function loadstopCallback(event) {
+                console.log('Loading finished: ' + event.url)
+            }
+
+            function loaderrorCallback(error) {
+                console.log('Loading error: ' + error.message)
+            }
+
+            function exitCallback() {
+                console.log('Browser is closed...')
+            }
+        }
     }
 })();
 
